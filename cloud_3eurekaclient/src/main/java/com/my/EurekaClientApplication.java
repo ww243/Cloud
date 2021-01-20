fe30ec50f66a36238526f0ad9f9e2f72ebb4a67f
ww243 <2500494776@qq.com>
2021-01-20 18:23:24 +0800

    ljn:1

---
+++ cloud_3eurekaclient/src/main/java/com/my/EurekaClientApplication.java
@@ -0,0 +1,18 @@
+package com.my;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientApplication {
+    public static void main( String[] args ) {
+        SpringApplication.run(EurekaClientApplication.class,args);
+        System.out.println( "Hello World!" );
+    }
+}
