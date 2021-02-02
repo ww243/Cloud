34f8e92ae1a8b2483e4d3e034ea7a5d3398ddacc
ww243 <2500494776@qq.com>
2021-02-02 16:46:20 +0800

    ljn:8

--- cloud_13configserver/src/main/java/com/my/ConfigserverApplication.java
+++ cloud_13configserver/src/main/java/com/my/ConfigserverApplication.java
@@ -2,6 +2,7 @@ package com.my;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
@@ -13,6 +14,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableConfigServer //开启统一配置中心服务
+@ServletComponentScan("com.my.filters")
 public class ConfigserverApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigserverApplication.class,args);
--- cloud_14configclient/src/main/java/com/my/controller/TestController.java
+++ cloud_14configclient/src/main/java/com/my/controller/TestController.java
@@ -1,7 +1,10 @@
 package com.my.controller;
 
+import jdk.nashorn.internal.ir.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,15 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("text")
+@RefreshScope //远端配置更新之后立即更新当前运行class中配置信息
 public class TestController {
 
     private static final Logger log = LoggerFactory.getLogger(TestController.class);
 
+    @Value("${name}")
+    private String name;
 
     @RequestMapping("text")
     public String text(){
         log.info("text......");
-
-        return "ok";
+        log.info("name:{}",name);
+        return "ok "+name;
     }
 }
